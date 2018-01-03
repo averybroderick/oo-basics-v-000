ab55ad381700d83b01df373525baf348a50bfe89
Avery Broderick <avery.c.broderick@gmail.com>
2018-01-03 22:18:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,35 +1,13 @@
 class Book
 
-  def initialize(title)
+ attr_accessor :author, :page_count, :genre
+
+ def initialize(title)
    @title = title
-  end
-
-  def title
-    @title = "And Then There Were None"
-  end
-
-  def author=(the_author)
-    @author = the_author
-  end
-
-  def author
-    @author = "Agatha Christie"
-  end
-
- def page_count
-   @page_count = 272
  end
 
- def page_count=(num)
-   @page_count = num
- end
-
- def genre
-   @genre = "Mystery"
- end
-
- def genre=(gen)
-   @genre = gen
+ def title
+   @title
  end
 
  def turn_page
